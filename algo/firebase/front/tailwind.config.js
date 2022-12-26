8b729bc69a161187f18b901970943da0ddc05ea4
ryohei-ebisawa <xebisawa.ryohei@contract.isid.co.jp>
2022-12-26 11:22:38 +0900

    Fix _app.tsx, css

--- algo/firebase/front/tailwind.config.js
+++ algo/firebase/front/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily : {
+      sans: ['"Noto Sans JP"'],
+    },
     extend: {
       colors: {
         "color-green": "#55CAC3",
