e4f6b36e622434eaf86892ab9759f2b9e6bd63d2
ryohei-ebisawa <xebisawa.ryohei@contract.isid.co.jp>
2023-02-08 13:38:15 +0900

    bugfix 2.1,3.1,4.1 change link from next/link to a

--- algo/firebase/front/src/components/common/ListConfirmLink/ConfirmLink.tsx
+++ algo/firebase/front/src/components/common/ListConfirmLink/ConfirmLink.tsx
@@ -13,23 +13,20 @@ const ConfirmLink = ({ actor, type }: ListConfirmLinkParam) => {
     const bgColor = getBgColor();
 
     return (
-        <Link href={url}>
-            <a className={`flex flex-col justify-center items-center gap-[6px] ${(actor === "applier" ? "w-[139px]" : "w-[246px]")} h-[89px] rounded-lg ${bgColor}`}>
-                <img src={
-                    type === "application"
-                        ? "/application-icon.svg"
-                        : "/VC-icon.svg"
-                } alt="アイコン"
-                    className={(type === "application" ? "pl-[6px]" : "")}
-                />
-
-                {
-                    type === "application"
-                        ? <span className="mt-[6px] text-center text-[15px] leading-[15px] font-bold">申請一覧</span>
-                        : <span className={`text-center ${(actor === "applier" ? "text-[12px]" : "text-[15px]")} font-bold`}>デジタル証明書{(actor === "applier" ? <br /> : null)}(VC)一覧</span>
-                }
-            </a>
-        </Link>
+        <a href={url} className={`flex flex-col justify-center items-center gap-[6px] ${(actor === "applier" ? "w-[139px]" : "w-[246px]")} h-[89px] rounded-lg ${bgColor}`}>
+            <img src={
+                type === "application"
+                    ? "/application-icon.svg"
+                    : "/VC-icon.svg"
+            } alt="アイコン"
+                className={(type === "application" ? "pl-[6px]" : "")}
+            />
+            {
+                type === "application"
+                    ? <span className="mt-[6px] text-center text-[15px] leading-[15px] font-bold">申請一覧</span>
+                    : <span className={`text-center ${(actor === "applier" ? "text-[12px]" : "text-[15px]")} font-bold`}>デジタル証明書{(actor === "applier" ? <br /> : null)}(VC)一覧</span>
+            }
+        </a>
     )
 };
 
--- algo/firebase/front/src/components/common/StepLink/StepLink.tsx
+++ algo/firebase/front/src/components/common/StepLink/StepLink.tsx
@@ -15,13 +15,10 @@ const StepLink = ({ step, onClick }: StepLinkParams) => {
                 <div className="flex justify-center items-center w-[53px] h-[53px] rounded-full bg-color-step-icon z-10">
                     <span className="text-[13px] text-color-white font-bold">STEP{step}</span>
                 </div>
-                <Link href={getUrl()}>
-                    <a onClick={onClick} className="flex justify-center items-center w-[180px] h-[47px] mt-4 -ml-[5px] mr-5 bg-white border border-color-menu-button rounded-lg z-0">
+                    <a href={getUrl()} onClick={onClick} className="flex justify-center items-center w-[180px] h-[47px] mt-4 -ml-[5px] mr-5 bg-white border border-color-menu-button rounded-lg z-0">
                         <span className={"text-[15px] font-bold"}>{getLabel()}</span>
                     </a>
-                </Link>
             </div>
-
         </>
     )
 };
